213a7fac06ac0ee5226d384cf1ed200cd058c6a8
Cheevavorn Saitthakool <mean.mea2@gmail.com>
2019-03-14 23:24:16 +0700

    commit web draft structure

--- index.php
+++ index.php
@@ -1,2 +1,31 @@
-<?php
-  echo "<h1>CRM BU</h1>";
+<?php require('./utils/array_utils.php'); ?>
+<?php require('./partials/header.php'); ?>
+<?php 
+  // check 'action' from get params
+  if(!array_key_exists("action", $_GET)){
+    header("Location: ?action=home");
+    exit(0);
+  }
+
+  // get action key to action param
+  $action = $_GET['action'];
+
+  // filter only php extension
+  $filter_file_name = array_filter(scandir("./"), "filter_php_file");
+
+  $page_php_path = "{$action}.php";
+  if(in_array($page_php_path, $filter_file_name, true))
+    include($page_php_path);
+  else
+    include("404.php");
+  
+  // include essentails scripts 
+  require("./partials/scripts.php");
+
+  // include script control withit page
+  $page_script_path = "scripts/{$action}.js";
+  if(file_exists($page_script_path))
+    include($page_script_path);
+  
+?>
+<?php require("./partials/footer.php"); ?>
---
+++ partials/header.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="th">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>Title of the document</title>
+    <link rel="stylesheet" href="./assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="./assets/css/theme_1545570683953.css">
+    <link href="https://fonts.googleapis.com/css?family=Sarabun|Roboto" rel="stylesheet">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+    <style>
+      * {
+        font-family: 'Sarabun', 'Roboto', sans-serif;
+      }
+    </style>
+  </head>
+
+  <body>
+    <header style="margin-bottom: 70px;">
+      <!-- Image and text -->
+      <nav class="navbar fixed-top navbar-light bg-light">
+        <div class="container">
+          <a class="navbar-brand font-weight-bold" href="#!">
+            <img src="./assets/images/pea-logo.png" width="100" class="d-inline-block align-top" alt="">
+            Customer Relationship Management (Business Unit)
+          </a>
+        </div>
+      </nav>
+    </header>
+    <main>
+      <div class="container">
+      <div class="row">
+        <div class="col-sm-12 col-md-4 col-lg-3">
+          <h1 class="dropdown-header font-weight-bold text-center">เมนูหลัก</h1>
+          <div class="nav flex-column nav-pills border border-primary rounded shadow-sm" id="v-pills-tab" role="tablist" aria-orientation="vertical">
+            <a class="nav-link active" id="v-pills-home-tab" data-toggle="pill" href="#v-pills-home" role="tab" aria-controls="v-pills-home" aria-selected="true">
+              <i class="fas fa-home"></i>
+              หน้าแรก
+            </a>
+            <!-- <div class="dropdown-divider"></div> -->
+            <a class="nav-link" id="v-pills-profile-tab" data-toggle="pill" href="#v-pills-profile" role="tab" aria-controls="v-pills-profile" aria-selected="false">
+              <i class="fas fa-star"></i>
+              ข้อมูลลูกค้า High Value
+            </a>
+            <a class="nav-link" id="v-pills-messages-tab" data-toggle="pill" href="#v-pills-messages" role="tab" aria-controls="v-pills-messages" aria-selected="false">
+              <i class="fab fa-searchengin"></i>
+              ค้นหาลูกค้าอัตโนมัติ
+            </a>
+            <a class="nav-link" id="v-pills-settings-tab" data-toggle="pill" href="#v-pills-settings" role="tab" aria-controls="v-pills-settings" aria-selected="false">
+              <i class="fas fa-hand-point-up"></i>  
+              ค้นหาลูกค้ากำหนดเงื่อนไขเอง
+            </a>
+          </div>
+        </div>
+        <div class="col-sm-12 col-md-8 col-lg-9">
