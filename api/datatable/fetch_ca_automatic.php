e4ea87111e3adaba1a1ab11b3505b82d8e2b793d
Cheevavorn Saitthakool <mean.mea2@gmail.com>
2019-03-19 15:28:35 +0700

    test datatable data

---
+++ api/datatable/fetch_ca_automatic.php
@@ -0,0 +1,9 @@
+<?php
+  // require('../../utils/');
+  echo json_encode([
+    ['id' => '1', 'ca'=>'00213123123','bp'=>'123457','bp_name'=>'ชื่อของ BP','bp_address'=>'ที่อยู่ของ BP'],
+    ['id' => '1', 'ca'=>'00213123123','bp'=>'123457','bp_name'=>'ชื่อของ BP','bp_address'=>'ที่อยู่ของ BP'],
+    ['id' => '1', 'ca'=>'00213123123','bp'=>'123457','bp_name'=>'ชื่อของ BP','bp_address'=>'ที่อยู่ของ BP'],
+    ['id' => '1', 'ca'=>'00213123123','bp'=>'123457','bp_name'=>'ชื่อของ BP','bp_address'=>'ที่อยู่ของ BP']
+  ],
+  JSON_UNESCAPED_UNICODE);
--- search.php
+++ search.php
@@ -1,1 +1,26 @@
-Hello from search menu
+<div class="row">
+  <!-- <h6 class="p-3 font-weight-bold pt-3 rounded-pill bg-primary text-white"> -->
+  <h4 class="p-3 font-weight-bold">
+    <i class="fas fa-search"></i> ค้นหาข้อมูลลูกค้ารายสำคัญ
+  </h4>
+  <!-- </h6> -->
+</div>
+<div class="row">
+  <div class="col-sm-12 col-md-12 col-lg-12">
+    <table data-toggle="table" data-url="./api/datatable/fetch_ca_automatic.php">
+      <thead>
+        <tr>
+          <th data-field="id">#</th>
+          <th data-field="ca">หมายเลข CA</th>
+          <th data-field="bp">หมายเลข BP</th>
+          <th data-field="bp_name">ชื่อ BP</th>
+          <th data-field="bp_address">ที่อยู่ BP</th>
+        </tr>
+      </thead>
+    </table>
+  </div>
+</div>
+<script>
+  
+</script>
+<!-- <div class="dropdown-divider"></div> -->
