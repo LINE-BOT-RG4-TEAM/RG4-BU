b9aca458ff5d8881d2cfd2d2f7af273a7c43e081
Cheevavorn Saitthakool <mean.mea2@gmail.com>
2019-03-19 15:50:22 +0700

    add search feature

--- api/datatable/fetch_ca_automatic.php
+++ api/datatable/fetch_ca_automatic.php
@@ -1,9 +1,23 @@
 <?php
-  // require('../../utils/');
-  echo json_encode([
-    ['id' => '1', 'ca'=>'00213123123','bp'=>'123457','bp_name'=>'ชื่อของ BP','bp_address'=>'ที่อยู่ของ BP'],
-    ['id' => '1', 'ca'=>'00213123123','bp'=>'123457','bp_name'=>'ชื่อของ BP','bp_address'=>'ที่อยู่ของ BP'],
-    ['id' => '1', 'ca'=>'00213123123','bp'=>'123457','bp_name'=>'ชื่อของ BP','bp_address'=>'ที่อยู่ของ BP'],
-    ['id' => '1', 'ca'=>'00213123123','bp'=>'123457','bp_name'=>'ชื่อของ BP','bp_address'=>'ที่อยู่ของ BP']
-  ],
-  JSON_UNESCAPED_UNICODE);
+  require('../../utils/db_connector.php');
+
+  $fetch_automatic = "
+    SELECT ca.CA
+          ,ca.BP
+          ,bp.CUSTOMER_NAME AS 'BP_NAME'
+          ,ca.ADDRESS AS 'CA_ADDRESS'
+          ,kW_PK
+          ,MAX_BILL
+          ,AVG_BILL
+          ,kVA_SIZE
+          ,COUNT(history.CA) AS 'quantity_purchase'
+    FROM crm_bu.ca 
+    JOIN crm_bu.bp ON ca.bp = bp.BP
+    JOIN crm_bu.history ON ca.CA = history.CA
+      GROUP BY history.CA
+      ORDER BY COUNT(history.CA) DESC
+  ";
+
+  $fetch_automatic_query = $conn->query($fetch_automatic);
+  $ca_obj = $fetch_automatic_query->fetch_all(MYSQLI_ASSOC);
+  echo json_encode($ca_obj, JSON_UNESCAPED_UNICODE);
--- search.php
+++ search.php
@@ -10,17 +10,40 @@
     <table data-toggle="table" data-url="./api/datatable/fetch_ca_automatic.php">
       <thead>
         <tr>
-          <th data-field="id">#</th>
-          <th data-field="ca">หมายเลข CA</th>
-          <th data-field="bp">หมายเลข BP</th>
-          <th data-field="bp_name">ชื่อ BP</th>
-          <th data-field="bp_address">ที่อยู่ BP</th>
+          <th>#</th>
+          <th data-field="CA"><i class="fas fa-indent"></i> หมายเลข CA</th>
+          <th data-field="BP"><i class="fas fa-business-time"></i> หมายเลข BP</th>
+          <th data-field="BP_NAME"><i class="fas fa-user-tie"></i> ชื่อ BP</th>
+          <th data-field="quantity_purchase"><i class="fas fa-receipt"></i> จำนวนครั้งการซื้อธุรกิจเสริม</th>
+          <th data-formatter="operateFormatter" data-events="operateEvents"><i class="fas fa-cogs"></i> จัดการ</th>
         </tr>
       </thead>
     </table>
   </div>
 </div>
 <script>
-  
+  function operateFormatter(value, row, index) {
+    return [
+      '<a class="like" href="javascript:void(0)" title="Like">',
+      '<i class="fa fa-heart"></i>',
+      '</a>  ',
+      '<a class="remove" href="javascript:void(0)" title="Remove">',
+      '<i class="fa fa-trash"></i>',
+      '</a>'
+    ].join('');
+  }
+
+  window.operateEvents = {
+    'click .like': function (e, value, row, index) {
+      console.log(e, value, row, index);
+      alert('You click like action, row: ' + JSON.stringify(row))
+    },
+    'click .remove': function (e, value, row, index) {
+      $("table").bootstrapTable('remove', {
+        field: 'id',
+        values: [row.id]
+      })
+    }
+  }
 </script>
 <!-- <div class="dropdown-divider"></div> -->
