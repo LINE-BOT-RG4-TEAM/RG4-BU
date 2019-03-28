95cdb4cad30d64b70710cde68ca497f465e59516
Nutthapong <tue_seed@hotmail.com>
2019-03-28 20:36:02 +0700

    1561351

--- api/liff_construc_api.php
+++ api/liff_construc_api.php
@@ -8,7 +8,7 @@ $sql_level_1 = "SELECT * FROM product_category WHERE cate_id = '$cate_id'";
 $query_level_1 = mysqli_query($conn,$sql_level_1);
 $obj_level = mysqli_fetch_assoc($query_level_1);
 $data1 = array("level"=>$obj_level);
-array_push($data,$obj_level);
+array_push($data,$data1);
 /////////
 $sql_cate_id = "SELECT * FROM product_category WHERE parent_cate_id = '$cate_id'";
 $query_cate_id = mysqli_query($conn,$sql_cate_id);
