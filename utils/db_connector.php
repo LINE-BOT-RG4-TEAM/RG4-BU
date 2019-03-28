41d79bca96d374f2c13b6013664851c5e543f99b
Cheevavorn Saitthakool <mean.mea2@gmail.com>
2019-03-28 16:44:06 +0700

    fix db connector

--- utils/db_connector.php
+++ utils/db_connector.php
@@ -3,11 +3,14 @@
   ini_set('display_errors', 1);
 	error_reporting(~0);
 
-  $serverName = "172.30.130.167";
-  $userName = "dev";
-  $userPassword = "dev";
-  $dbName = "crm_bu";
-  $db_port = '3306';
+  $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
 
-	$conn = mysqli_connect($serverName, $userName, $userPassword, $dbName, $db_port) or die('Unable to establish a CRM_BU connection');
+  $server = $url["host"];
+  $username = $url["user"];
+  $password = $url["pass"];
+  $db = substr($url["path"], 1);
+
+  $conn = new mysqli($server, $username, $password, $db);
+
+	// $conn = mysqli_connect($serverName, $userName, $userPassword, $dbName, $db_port) or die('Unable to establish a CRM_BU connection');
   $conn->set_charset("utf8");
