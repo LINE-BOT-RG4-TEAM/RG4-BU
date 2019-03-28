76602b75d193c4543cf1cdbfde770df1c8f53164
Cheevavorn Saitthakool <mean.mea2@gmail.com>
2019-03-28 11:28:27 +0700

    เพิ่มหน้า template สำหรับการ shopping ลูกค้า

---
+++ cust_home.php
@@ -0,0 +1,15 @@
+<div class="row">
+  <div class="col-sm-12 col-md-12 col-lg-8 offset-xl-1 col-xl-5 text-right">
+    <div style="height: 15%"></div>
+    <div style="height: 75%">
+        <h1 class="align-middle font-weight-bold">PEA Marketplace</h1>
+        <p class="align-middle text-grey">Shop a PEA service with best service</p>
+    </div>
+  </div>
+  <div class="col-sm-12 col-md-12 col-lg-4 col-xl-6">
+    <img class="d-none d-md-block" height="75%" src="https://marketplace-cdn.atlassian.com/s/public/AppsMirror-1-052ef62a1badc10cb07e1acb207ac2d1.svg" alt="">
+  </div>
+</div>
+<div class="row bg-light">
+  replace card here.
+</div>
