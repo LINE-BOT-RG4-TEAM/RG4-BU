b564fb6048a5d4efb91d96d883326c3d573ab031
Nutthapong <tue_seed@hotmail.com>
2019-03-28 20:29:55 +0700

    11

--- api/liff_construc_api.php
+++ api/liff_construc_api.php
@@ -1,9 +1,16 @@
 <?php
 require('../utils/db_connector.php');
+$data =array();
 $cate_id = $_GET["cate_id"];
+/////////
+$sql_level_1 = "SELECT * FROM product_category WHERE cate_id = '$cate_id'";
+$query_level_1 = mysqli_query($conn,$sql_level_1);
+$obj_level = mysqli_fetch_assoc($query_level_1);
+array_push($data,$obj_level);
+/////////
 $sql_cate_id = "SELECT * FROM product_category WHERE parent_cate_id = '$cate_id'";
 $query_cate_id = mysqli_query($conn,$sql_cate_id);
-$data =array();
+
 while($obj = mysqli_fetch_assoc($query_cate_id))
 {
     array_push($data,$obj);
