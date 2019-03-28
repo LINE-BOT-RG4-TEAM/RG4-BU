a306f3b1816acb3fb23c1b4b26a02d8f9add4f8c
Cheevavorn Saitthakool <mean.mea2@gmail.com>
2019-03-28 11:52:12 +0700

    แก้ไขหน้าเลือกสินค้า marketplace และเพิ่มหน้าลงทะเบียน

--- cust_home.php
+++ cust_home.php
@@ -1,13 +1,13 @@
 <div class="row">
   <div class="col-sm-12 col-md-12 col-lg-8 offset-xl-1 col-xl-5 text-right">
-    <div style="height: 15%"></div>
-    <div style="height: 75%">
-        <h1 class="align-middle font-weight-bold">PEA Marketplace</h1>
-        <p class="align-middle text-grey">Shop a PEA service with best service</p>
+    <div style="height: 20%"></div>
+    <div style="height: 40%">
+        <h1 class="text-sm-center text-md-center text-lg-right font-weight-bold">PEA Marketplace</h1>
+        <p class="text-sm-center text-md-center text-lg-right text-grey">Shop a PEA service with best service</p>
     </div>
   </div>
   <div class="col-sm-12 col-md-12 col-lg-4 col-xl-6">
-    <img class="d-none d-md-block" height="75%" src="https://marketplace-cdn.atlassian.com/s/public/AppsMirror-1-052ef62a1badc10cb07e1acb207ac2d1.svg" alt="">
+    <img class="d-none d-lg-block h-75" src="https://marketplace-cdn.atlassian.com/s/public/AppsMirror-1-052ef62a1badc10cb07e1acb207ac2d1.svg" alt="">
   </div>
 </div>
 <div class="row bg-light">
---
+++ cust_register.php
@@ -0,0 +1,5 @@
+<div class="row">
+  <h2 class="text-center font-weight-bold">
+    ลงทะเบียนการใช้บริการธุรกิจเสริมการไฟฟ้า
+  </h2>
+</div>
