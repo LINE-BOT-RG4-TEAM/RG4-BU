e4af9a89f71f3bbcd5654e494504efec2cbf3031
Cheevavorn Saitthakool <mean.mea2@gmail.com>
2019-04-02 22:40:18 +0700

    fix encode

--- api/check_registered_status.php
+++ api/check_registered_status.php
@@ -12,14 +12,17 @@
   $stmt->bind_param("s", $userId);
   $stmt->execute();
   if($stmt->error) {
-    http_response_code(503);
+    // http_response_code(503);
+    echo json_encode(['status'=>'503']);
     exit(1);
   }
 
   if($stmt->num_rows > 0) {
-    http_response_code(200);
+    // http_response_code(200);
+    echo json_encode(['status'=>'200']);
     exit(0);
   } else {
-    http_response_code(404);
+    // http_response_code(404);
+    echo json_encode(['status'=>'404']);
     exit(1);
   }
