2a7e7074a22fc75c666ac79886eecf5023ce2335
Nutthapong <tue_seed@hotmail.com>
2019-04-04 21:17:28 +0700

    qwertyuio

--- api/add_2_cart.php
+++ api/add_2_cart.php
@@ -4,8 +4,8 @@ $UserID = $_POST["userid"];
 $cate_id = $_POST["cate_id"];
 $comment = $_POST["comment"];
 //////check ใบสั่งซื้อที่มีสถานะ A
-/*$sql_check_purchase_id_a = "SELECT MAX(PURCHASE_ID) as l_purchase FROM purchase WHERE UserID = '".$UserID".' AND PURCHASE_STATUS = 'A'";
-$query_check = mysqli_query($conn,$sql_check_purchase_id_a);
+$sql_check_purchase_id_a = "SELECT MAX(PURCHASE_ID) as l_purchase FROM purchase WHERE UserID = '".$UserID".' AND PURCHASE_STATUS = 'A'";
+/*$query_check = mysqli_query($conn,$sql_check_purchase_id_a);
 $obj_check = mysqli_fetch_assoc($query_check);
 
 ///// check ใบสั่งซื้อที่มีสถานะ C
