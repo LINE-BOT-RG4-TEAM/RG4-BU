e2f0fe4ae34069507fd801249190f22e19f7ec69
Nutthapong <tue_seed@hotmail.com>
2019-04-04 21:54:27 +0700

    qwerfdsa

--- api/add_2_cart.php
+++ api/add_2_cart.php
@@ -34,8 +34,9 @@ if($obj_check["l_purchase"] == null && $obj_check_c["l_purchase"] == null)
 }
 else if($obj_check["l_purchase"] <> null)
 {
-   /* $sql_insertlineitem = "INSERT INTO purchase_lineitem(purchase_id,cate_id,des) VALUES('$obj_check["l_purchase"]','$cate_id','$comment')";
-    mysqli_query($conn,$sql_insertlineitem);*/
+    $purchase_id = $obj_check["l_purchase"];
+    $sql_insertlineitem = "INSERT INTO purchase_lineitem(purchase_id,cate_id,des) VALUES('$purchase_id','$cate_id','$comment')";
+    mysqli_query($conn,$sql_insertlineitem);
     echo "if 2";
 }/*
 else if($obj_check_c["l_purchase"] <> null)
