7563d88dcb749f4062ba635c6fd39be0cc012932
Nutthapong <tue_seed@hotmail.com>
2019-05-23 21:41:34 +0700

    line check for modal 2141 แก้แล้วแก้อีก

--- api/check_service_api.php
+++ api/check_service_api.php
@@ -18,6 +18,7 @@
     }
     else if($item == 'modal')
     {
+        $data =array();
         $sql_check_purchase_id_a = "SELECT MAX(PURCHASE_ID) as l_purchase FROM purchase WHERE UserID = '$UserID' AND PURCHASE_STATUS = 'A'";
         $query_check = mysqli_query($conn,$sql_check_purchase_id_a);
         $obj_check = mysqli_fetch_assoc($query_check);
@@ -25,8 +26,11 @@
 
         $sql_select_lineitem = "SELECT * FROM purchase_lineitem WHERE purchase_id = '$purchase_id'";
         $lineitem = mysqli_query($conn,$sql_select_lineitem);
-        $obj = mysqli_fetch_assoc($lineitem);
-        echo $obj;
+        while($obj = mysqli_fetch_assoc($lineitem))
+        {
+            array_push($data,$obj);
+        }
+        echo json_encode($data);
 
     }
     
