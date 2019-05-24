901e47265786fbf9bcd871e079a8aab3adc998a2
Nutthapong <tue_seed@hotmail.com>
2019-05-24 08:45:40 +0700

    sadsawd

--- scripts/liff_global.js
+++ scripts/liff_global.js
@@ -58,7 +58,7 @@ function check_lineitem()
               {
                 alert("check lineitem success");
                 var obj = JSON.parse(response) || {};
-                var html_text = render_lineitem(obj);
+                //var html_text = render_lineitem(obj);
                 alert(obj[0].cate_id);
                 $("#lineitem_area").html(html_text);
               }				
