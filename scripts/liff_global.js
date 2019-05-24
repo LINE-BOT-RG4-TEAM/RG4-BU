322990080e4fd5d3a053a0f575280af39e023a41
Nutthapong <tue_seed@hotmail.com>
2019-05-24 09:04:46 +0700

    sadadasd

--- scripts/liff_global.js
+++ scripts/liff_global.js
@@ -59,7 +59,7 @@ function check_lineitem()
                 alert("check lineitem success");
                 var obj = JSON.parse(response) || {};
                 //var html_text = render_lineitem(obj);
-                alert(obj[0].cate_id);
+                alert(obj);
                 $("#lineitem_area").html(html_text);
               }				
     });
