3ee4588c48a80f039d7c29e4d56986eda78f52f3
Nutthapong <tue_seed@hotmail.com>
2019-05-27 23:27:22 +0700

    ลืมๆแก้ uid dummy

--- scripts/liff_global.js
+++ scripts/liff_global.js
@@ -12,8 +12,8 @@ window.onload = function(e) {
   input.setAttribute("type", "hidden");
   input.setAttribute("name", "userId");
   input.setAttribute("id", "userId");
-  //input.setAttribute("value", data.context.userId);
-  input.setAttribute("value", 'Uaef7a8e9eedce02d663bf83aec1dd910555');
+  input.setAttribute("value", data.context.userId);
+  //input.setAttribute("value", 'Uaef7a8e9eedce02d663bf83aec1dd910555');
   document.getElementsByTagName("body")[0].append(input);
   quantity_service();
 };
