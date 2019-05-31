d2f0306ca139af802f6f07037a735a11873a3df0
Cheevavorn Saitthakool <mean.mea2@gmail.com>
2019-05-31 09:38:24 +0700

    fix url

--- authorize.php
+++ authorize.php
@@ -1,20 +1,20 @@
 <?php
-    $authorizeURL = "https://notify-bot.line.me/oauth/authorize";
+    function siteURL(){
+        $protocol = (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off' || $_SERVER['SERVER_PORT'] == 443) ? "https://" : "http://";
+        $domainName = $_SERVER['HTTP_HOST'].'/';
+        return $protocol.$domainName;
+    }
+
     define('CLIENT_ID', 'YQz4zuElk6zePoTWmsOte7');
     define('CLIENT_SECRET', 'ArleV3DuZ4rpFX0I6v2Utb879SjGAo4s46za8jSaY5g');
-    // $client_id = "YQz4zuElk6zePoTWmsOte7";
-    // $client_secret = "ArleV3DuZ4rpFX0I6v2Utb879SjGAo4s46za8jSaY5g";
-    // header("Location: https://notify-bot.line.me/oauth/authorize");
-    // header("'response_type': 'code'");
-    // header("'client_id': 'YQz4zuElk6zePoTWmsOte7'");
-    // header("'redirect_uri': https://monstro.serveo.net/crm-bu/success.php");
-    // header("'scope': 'notify'");
-    // header("'state': 'cheevavorn'");
+    define('SITE_URL', siteURL());
+    $authorizeURL = "https://notify-bot.line.me/oauth/authorize";
 
     $params = array(
         'response_type' => 'code',
         'client_id' => CLIENT_ID,
-        'redirect_uri' => 'https://pea-crm.herokuapp.com/callback.php',
+        'redirect_uri' => SITE_URL."callback.php",
+        // 'redirect_uri' => SITE_URL."crm-bu/"."callback.php",
         'scope' => 'notify',
         'state' => 'cheevavorn'
     );
