8b6bb3c6a1e5d49abc27e10985e17192063b48d8
Cheevavorn Saitthakool <mean.mea2@gmail.com>
2019-06-06 15:53:02 +0700

    add mpdf libs

--- show_invoice.php
+++ show_invoice.php
@@ -1,4 +1,12 @@
 <?php
+    require_once __DIR__ . '/vendor/autoload.php';
+
     $queryString = $_SERVER['QUERY_STRING'];
     parse_str(base64_decode($queryString), $params);
-    var_dump($params);
+
+    $purchase_id = $params['purchase_id'];
+
+    $mpdf = new \Mpdf\Mpdf();
+    $mpdf->WriteHTML('<h1>Hello world!</h1>');
+    $mpdf->WriteHTML("purchase id is $purchase_id");
+    $mpdf->Output();
