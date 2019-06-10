54a281fdb92f4f6b45551f2ba0ab666d484c488b
Cheevavorn Saitthakool <mean.mea2@gmail.com>
2019-06-10 23:47:15 +0700

    fix pdf document

--- show_invoice.php
+++ show_invoice.php
@@ -3,18 +3,62 @@
     require_once './utils/db_connector.php';
 
     $queryString = $_SERVER['QUERY_STRING'];
-    parse_str(base64_decode($queryString), $params);
+    if(!isset($queryString) || empty($queryString)){
+        die("ไม่สามารถเรียกดูเอกสารหลักฐานได้เนื่องจากท่านเรียกดูด้วยวิธีการที่ไม่ถูกต้อง");
+    }
 
+    parse_str(base64_decode($queryString), $params);
+    if(empty(array_filter($params))) {
+        die("ไม่พบข้อมูลการสั่งซื้อของท่าน, กรุณาลองใหม่อีกครั้ง");
+    }
     $purchase_id = $params['purchase_id'];
 
-    // $conn->query
+    // ดึงข้อมูลทั่วไปมาแสดงบริเวณ Header
+    $fetch_general_data = "
+        SELECT bp.BP
+                , bp.CUSTOMER_NAME
+                , ca.PEA_CODE
+                , ADDRESS
+                , FullName
+                , CA_TEL
+                , CA_EMAIL
+                , purchase.UserID
+        FROM purchase 
+            JOIN ca ON ca.UserID = purchase.UserID
+            JOIN bp ON ca.bp = bp.BP
+        WHERE purchase_id = '$purchase_id';
+    ";
+    $general_results = $conn->query($fetch_general_data);
+    $row = $general_results->fetch_assoc();
+
+    // ดึงรายการสินค้าบริการต่างๆ ที่ได้รับ อ้างอิงจาก purchase id
+    $fetch_lineitem = "
+        SELECT lineitem.cate_id
+            , cate_name
+            , CASE
+                WHEN LENGTH(des) = 0 THEN '--ท่านไม่มีความประสงค์เพิ่มเติม--'
+                ELSE des
+            END AS `description`
+            , lineitem.appointment_date
+        FROM purchase_lineitem lineitem
+            JOIN product_category category ON lineitem.cate_id = category.cate_id
+        WHERE lineitem.purchase_id = '$purchase_id';
+    ";
+    $lineitem_results = $conn->query($fetch_lineitem);
 
     $mpdf = new \Mpdf\Mpdf([
         'mode' => 'utf-8', 
         'orientation' => 'P',
-        'default_font' => 'sarabun'
+        'default_font' => 'sarabun',
+        'setAutoBottomMargin' => 'pad'
     ]);
+    
+    // set watermarks
+    $wm = ("การไฟฟ้าส่วนภูมิภาค - กฟภ.");
+    $mpdf->SetWatermarkText($wm, 0.1);
+    $mpdf->showWatermarkText = true;
 
+    // set title and author
     $current_date = date("Y-m-d");
     $mpdf->SetTitle("$current_date-เอกสารหลักฐานการสั่งซื้อ-$purchase_id");
     $mpdf->SetAuthor("Automated report by SmartBiz");
@@ -30,10 +74,10 @@
         <tr>
             <td width="33%">
                 <img src="./assets/images/qr-code-with-logo.png" width="100" /><br/>
-                สร้างรายงานเมื่อ {DATE j-m-Y}
+                ระบบอัตโนมัติสร้างเอกสารนี้เมื่อวันที่ {DATE j-m-Y}
             </td>
             <td width="33%" align="center" style="vertical-align: bottom;">หน้าที่ {PAGENO} จากทั้งหมด {nbpg} หน้า</td>
-            <td width="33%" style="text-align: right;vertical-align: bottom;">การไฟฟ้าส่วนภูมิภาค</td>
+            <td width="33%" style="text-align: right;vertical-align: bottom;">คำสั่งซื้อ '.$purchase_id.'</td>
         </tr>
     </table>');
     $body_page = "
@@ -73,13 +117,13 @@
                         หมายเลขคำสั่งซื้อ:
                     </td>
                     <td style="font-size:22px;width:16%;padding-left:10px;border: 1px solid #E8E8E8;">
-                        #'.$purchase_id.'
+                        '.$purchase_id.'
                     </td>
                     <td style="font-size:24px;width:10%;text-align:right;font-weight:bold;">
                         ชื่อธุรกิจ:
                     </td>
                     <td style="font-size:20px;width:28%;padding-left:10px;border: 1px solid #E8E8E8;">
-                        บริษัท ซีพี จำกัด มหาชน
+                        '.$row['CUSTOMER_NAME'].'
                     </td>
                 </tr>
                 <tr>
@@ -87,7 +131,7 @@
                         ผู้สั่งซื้อ:
                     </td>
                     <td colspan="3" style="font-size:20px;width:26%;padding-left:10px;border: 1px solid #E8E8E8;">
-                        นายชีววร เศรษฐกุล
+                        '.$row['FullName'].'
                     </td>
                 </tr>
                 <tr>
@@ -95,7 +139,7 @@
                         ที่อยู่:
                     </td>
                     <td colspan="3" style="font-size:20px;width:26%;padding-left:10px;border: 1px solid #E8E8E8;">
-                        200 ถนนงามวงศ์วาน ลาดยาว จตุจักร กรุงเทพ 10900
+                        '.$row['ADDRESS'].'
                     </td>
                 </tr>
                 <tr>
@@ -103,13 +147,13 @@
                         เบอร์โทรศัพท์:
                     </td>
                     <td style="font-size:22px;width:16%;padding-left:10px;border: 1px solid #E8E8E8;">
-                        089-724-5303
+                        '.$row['CA_TEL'].'
                     </td>
                     <td style="font-size:24px;width:10%;text-align:right;font-weight:bold;">
                         อีเมล์:
                     </td>
                     <td style="font-size:20px;width:28%;padding-left:10px;border: 1px solid #E8E8E8;">
-                        mean.mea2@gmail.com
+                        '.$row['CA_EMAIL'].'
                     </td>
                 </tr>
             </table>
@@ -122,30 +166,30 @@
                         <th style="font-weight:bold;font-size:22px;color:white;">ลำดับ</th>
                         <th style="font-weight:bold;font-size:22px;color:white;">บริการ</th>
                         <th style="font-weight:bold;font-size:22px;color:white;">วันนัดหมาย</th>
-                        <th style="font-weight:bold;font-size:22px;color:white;">หมายเหตุ</th>
+                        <th style="font-weight:bold;font-size:22px;color:white;">ความประสงค์เพิ่มเติม</th>
                     </tr>
                 </thead>
                 <tbody>
-                    <tr>
-                        <td style="font-size:19px;" align="center">1</td>
-                        <td style="font-size:19px;white-space:nowrap;">M1301 - ทดสอบบริการ ทดสอบบริการ</td>
-                        <td align="center" style="font-size:19px;">3 พ.ย. 2562</td>
-                        <td style="font-size:19px;">กรุณาเข้ามาตรวจสอบในบริเวณโรงงานด้วยครับ</td>
-                    </tr>
-                    <tr>
-                        <td style="font-size:19px;" align="center">2</td>
-                        <td style="font-size:19px;white-space:nowrap;">M1302 - ทดสอบบริการ ทดสอบบริการ</td>
-                        <td align="center" style="font-size:19px;">3 พ.ย. 2562</td>
-                        <td style="font-size:19px;">กรุณาเข้ามาตรวจสอบในบริเวณโรงงานด้วยครับ</td>
-                    </tr>
-                </tbody>
+        ';
+        $i = 1;
+        while($lineitem = $lineitem_results->fetch_assoc()){
+            $body_page .= '
+                        <tr>
+                            <td style="font-size:19px;" align="center">'.($i++).'</td>
+                            <td style="font-size:17px;white-space:nowrap;">'.$lineitem['cate_id'].' - '.$lineitem['cate_name'].'</td>
+                            <td align="center" style="font-size:19px;">'.$lineitem['appointment_date'].'</td>
+                            <td style="font-size:19px;">'.$lineitem['description'].'</td>
+                        </tr>
+                        ';
+        }
+        $body_page .= '        </tbody>
             </table>
         </div>
         <div>
             <h3 style="font-family:printable4u;font-size:24px;">หมายเหตุ</h3>
             <ul style="font-size:20px;">
                 <li>
-                    ทาง กฟภ. ได้รับความต้องการของท่านเรียบร้อยแล้ว หากมีข้อมูลไม่ถูกต้อง ท่านสามารถแก้ไขได้ทันที ผ่าน LINE Account "PEA SmartBiz" หรือ QR Code บริเวณมุมล่างซ้ายค่ะ
+                    ทาง กฟภ. ได้รับความต้องการของท่านเรียบร้อยแล้ว หากมีข้อมูลไม่ถูกต้อง เช่น ชื่อ-สกุล, เบอร์โทรศัพท์, อีเมล์ หรือที่อยู่ ท่านสามารถแก้ไขได้ทันที ผ่าน LINE Account "PEA SmartBiz" หรือ QR Code บริเวณมุมล่างซ้ายค่ะ
                 </li>
             </ul>
         </div>
--- test.php
+++ test.php
@@ -17,7 +17,39 @@
     // echo "s3: ".sha1('s3');
 
     // $_GET
-    echo "<pre>";
-    // var_dump(array_keys($_GET));
-    echo json_encode($_POST, JSON_UNESCAPED_UNICODE);
-    echo "</pre>";
+    // echo "<pre>";
+    // // var_dump(array_keys($_GET));
+    // echo json_encode($_POST, JSON_UNESCAPED_UNICODE);
+    // echo "</pre>";
+    // $purchase_id = $_POST['purchase_id'];
+    // $set_pending_status_sql = "
+    //     UPDATE purchase 
+    //     SET PURCHASE_STATUS = 'P'
+    //     WHERE PURCHASE_ID = '$purchase_id';
+    // ";
+    // echo $set_pending_status_sql;
+
+    // define('STATUS_URI', 'https://notify-api.line.me/api/status');
+
+    // $headers = [
+    //     "Authorization: Bearer sMpjyLrk6WrFjgO0bboXxuZExejzYnsJG7oIyvZnUA3"
+    // ];
+
+    // $ch = curl_init();
+    // curl_setopt($ch, CURLOPT_URL, STATUS_URI);
+    // curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+    // curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    // curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+    // // curl_setopt($ch, CURLOPT_HEADER, 0);
+
+    // $res = curl_exec($ch);
+    // curl_close($ch);
+
+    // if ($res == false)
+    //     throw new Exception(curl_error($ch), curl_errno($ch));
+    
+    // $json = json_decode($res);
+    // $status = $json->status;
+    // var_dump($res);
+    echo $_SERVER['QUERY_STRING']."<br/>";
+    echo base64_encode($_SERVER['QUERY_STRING']);
