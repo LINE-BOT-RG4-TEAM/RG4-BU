ccd80aa2be56bbb7eb72fa90827d2c4ab5789aba
Cheevavorn Saitthakool <mean.mea2@gmail.com>
2019-07-10 18:48:40 +0700

    set environment db url production

--- utils/db_connector.php
+++ utils/db_connector.php
@@ -3,17 +3,17 @@
   ini_set('display_errors', 1);
 	error_reporting(~0);
 
-  /*$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+  $url = parse_url(getenv("CLEARDB_NAVY_URL"));
 
   $server = $url["host"];
   $username = $url["user"];
   $password = $url["pass"];
-  $db = substr($url["path"], 1);*/
+  $db = substr($url["path"], 1);
 
-  $server = 'us-cdbr-iron-east-03.cleardb.net';
-  $username = 'b1e0f1bc1c0da3';
-  $password = 'e615ba2a';
-  $db = 'heroku_bb297299e03d2d3';
+  // $server = 'us-cdbr-iron-east-03.cleardb.net';
+  // $username = 'b1e0f1bc1c0da3';
+  // $password = 'e615ba2a';
+  // $db = 'heroku_bb297299e03d2d3';
 
   $conn = new mysqli($server, $username, $password, $db);
 
