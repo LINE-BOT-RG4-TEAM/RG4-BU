32a5069e3c64fd7a4ba83d7a9015a419d1d513a0
Cheevavorn Saitthakool <mean.mea2@gmail.com>
2019-07-10 14:18:52 +0700

    increase size

--- scripts/liff_construc.js
+++ scripts/liff_construc.js
@@ -56,7 +56,7 @@ function render_card(obj)
         {
           var warranty =  'การรับประกัน :' + obj[i].warranty ;
         }
-        card = card + '<div class="col-sm-12 col-md-6 col-lg-4 mt-3"><div class="card h-100"><img class="card-img-top" src="images/'+ picture_name +'.jpg" alt="Card image"><div class="card-body"><h5 class="card-title">' +obj[i].cate_name + '</h5><p class="card-text">'+short_des+'</p><p class="card-text">' + warranty + '</p><img class="card-img-top" src="images/pea-price.jpg" alt="Card image">' + text_area + '</div><div class="card-footer"><a ' + href + ' class="btn btn-primary nav-link" ' + add2cart + '>'+button_label+'</a></div><div class="ribbon"><span>' + obj[i].cate_id + '</span></div></div></div>';
+        card = card + '<div class="col-sm-12 col-md-6 col-lg-4 mt-3"><div class="card h-100"><img class="card-img-top" src="images/'+ picture_name +'.jpg" alt="Card image"><div class="card-body"><h5 class="card-title">' +obj[i].cate_name + '</h5><p class="card-text">'+short_des+'</p><p class="card-text">' + warranty + '</p><img class="card-img-top" src="images/pea-price.jpg" alt="Card image">' + text_area + '</div><div class="card-footer"><a ' + href + ' class="btn btn-lg btn-primary nav-link" ' + add2cart + '>'+button_label+'</a></div><div class="ribbon"><span>' + obj[i].cate_id + '</span></div></div></div>';
         console.log(obj[i].cate_name);
         i++;
     }
