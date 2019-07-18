52f5360a29e9d256b9968b8247d048edb534910f
Cheevavorn Saitthakool <mean.mea2@gmail.com>
2019-07-18 23:59:58 +0700

    fix header of sub cate

--- liff_construc.php
+++ liff_construc.php
@@ -1,12 +1,13 @@
 <div class="row">
 <input type="hidden" name="hidden_cate_id" id="hidden_cate_id" value="<?=$_GET['cate_id']?>" />
-  <h6 class="p-3 font-weight-bold pt-3 rounded-pill bg-primary text-white" id="content_header">
-  </h6>
+  <h4 class="p-3 font-weight-bold pt-3 text-center text-primary" id="content_header">
+  <!-- <h6 class="p-3 font-weight-bold pt-3 rounded-pill bg-primary text-white" id="content_header"> -->
+  </h4>
 </div>
 <div class="row">
     <div class="col-sm-12 col-md-12 col-lg-12 mt-3">
         <div class="panel panel-primary">
-            <div class="p-2 panel-body border border-light text-justify" id="content_body" style="text-indent: 25px;">
+            <div class="p-2 panel-body border text-justify" id="content_body" style="text-indent: 25px;">
             
             </div>
         </div>
