d0f554e7d2449e115deb78edfcf70c2a0d9b8e24
Cheevavorn Saitthakool <mean.mea2@gmail.com>
2019-07-18 23:23:47 +0700

    ปรับตัวอักษรของการ์ดในหมวดหมู่

--- scripts/liff_service.js
+++ scripts/liff_service.js
@@ -8,7 +8,7 @@ function render_cate_card(obj) {
   var i = 0;
   var card = "";
   while(obj[i]) {
-    card = card + '<div class="col-sm-12 col-md-6 col-lg-4 mt-3"><div class="card h-100"><img class="card-img-top" src="images/'+ obj[i].picture_name +'.jpg" alt="Card image"><div class="card-body"><h5 class="card-title">' +obj[i].cate_name + '</h5><p class="card-text">'+obj[i].short_description+'</p></div><div class="card-footer"><a  href="?action=liff_construc&cate_id='+obj[i].cate_id+href+'" class="btn btn-primary nav-link">รายละเอียด</a></div></div></div>';
+    card = card + '<div class="col-sm-12 col-md-6 col-lg-4 mt-3"><div class="card h-100"><img class="card-img-top" src="images/'+ obj[i].picture_name +'.jpg" alt="Card image"><div class="card-body"><h5 class="card-title font-weight-bold"><i class="fas fa-caret-right"></i> ' +obj[i].cate_name + '</h5><p class="card-text">ข้อมูลเบื้องต้นของบริการ: '+obj[i].short_description+'</p></div><div class="card-footer"><a  href="?action=liff_construc&cate_id='+obj[i].cate_id+href+'" class="btn btn-primary nav-link">รายละเอียด</a></div></div></div>';
     // console.log(obj[i].cate_name);
     i++;
   }
