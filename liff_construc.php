210df4090868c0c8d2654b30fa8af0772753a6da
Cheevavorn Saitthakool <mean.mea2@gmail.com>
2019-07-19 00:08:04 +0700

    adjust header, border color and padding

--- liff_construc.php
+++ liff_construc.php
@@ -1,13 +1,15 @@
-<div class="row text-center">
+<div class="row">
     <input type="hidden" name="hidden_cate_id" id="hidden_cate_id" value="<?=$_GET['cate_id']?>" />
-  <h4 class="py-3 font-weight-bold text-primary" id="content_header">
-  <!-- <h6 class="p-3 font-weight-bold pt-3 rounded-pill bg-primary text-white" id="content_header"> -->
-  </h4>
+    <div class="text-center">
+        <h4 class="pt-3 pb-2 font-weight-bold text-primary" id="content_header">
+        <!-- <h6 class="p-3 font-weight-bold pt-3 rounded-pill bg-primary text-white" id="content_header"> -->
+        </h4>
+  </div>
 </div>
 <div class="row">
     <div class="col-sm-12 col-md-12 col-lg-12 mt-3">
         <div class="panel panel-primary">
-            <div class="p-4 panel-body border text-justify" id="content_body" style="text-indent: 25px;">
+            <div class="p-4 panel-body border border-primary text-justify" id="content_body" style="text-indent: 25px;">
             
             </div>
         </div>
