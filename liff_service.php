beb2fd5fc384d48d6dc6b4f3fbd1ca19d6e59531
Cheevavorn Saitthakool <mean.mea2@gmail.com>
2019-07-20 18:27:18 +0700

    create animated in header and subheader

--- liff_service.php
+++ liff_service.php
@@ -2,8 +2,8 @@
   <div class="col-sm-12 col-md-12 col-lg-8 offset-xl-1 col-xl-5">
     <div style="height: 20%"></div>
     <div style="height: 40%">
-      <h1 style="font-family: Roboto;" class="text-sm-center text-md-center text-lg-center text-xl-right font-weight-bold">PEA SmartBiz</h1>
-      <p style="font-family: Roboto;" class="text-muted text-sm-center text-md-center text-lg-center text-xl-right text-grey">Shop a PEA service with our best service</p>
+      <h1 style="font-family: Roboto;" class="animated fadeInLeft text-sm-center text-md-center text-lg-center text-xl-right font-weight-bold">PEA SmartBiz</h1>
+      <p style="font-family: Roboto;" class="animated fadeIn text-muted text-sm-center text-md-center text-lg-center text-xl-right text-grey">Shop a PEA service with our best service</p>
     </div>
   </div>
   <div class="col-sm-12 col-md-12 col-lg-4 col-xl-6">
