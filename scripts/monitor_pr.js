ef748a804d9a3252d13664ab63b2510d7caf8f1f
Cheevavorn Saitthakool <mean.mea2@gmail.com>
2019-07-21 23:25:50 +0700

    add animate text of pea_code

--- scripts/monitor_pr.js
+++ scripts/monitor_pr.js
@@ -41,41 +41,42 @@ $(function(){
             "font-wegiht": "bold",
             "font-size": "24px"
         })
-        .addClass("p-2 animated fadeInUp bg-light text-primary font-weight-bold");
+        .addClass("p-2 bg-light text-primary font-weight-bold");
+        $("tr.groupBy > td").addClass("animated fadeInUp");
     })
 });
 
-function getwithNo_cors(){
-    fetch('https://api.line.me/v2/bot/profile/U9629b9b72e84a8b4f4f904e21db16199', {	
-            'mode': 'no-cors',
-            headers: new Headers({
-                'Authorization': 'Bearer NCUUPMI9eL++OnIxuXLB4UFgn1I6BGR8MP6ljLJt2EWpNVRxjRZgfgMZdarheJ5d1Kd43O0V3RZkrsYs907kRbskDfAE9Rgmftpbbfy3ZbUX3JuIkDGtmlCgdfGkKoEUI3wNM0OEERB82/oyt4Kb0wdB04t89/1O/w1cDnyilFU='
-            })
-        }
-    )
-    .then(function(response){
-        console.log('getwithNo_cor - success', response.json());
-    })
-    .catch(function(error){
-        console.log('getwithNo_cor - error', error);
-    });
-}
+// function getwithNo_cors(){
+//     fetch('https://api.line.me/v2/bot/profile/U9629b9b72e84a8b4f4f904e21db16199', {	
+//             'mode': 'no-cors',
+//             headers: new Headers({
+//                 'Authorization': 'Bearer NCUUPMI9eL++OnIxuXLB4UFgn1I6BGR8MP6ljLJt2EWpNVRxjRZgfgMZdarheJ5d1Kd43O0V3RZkrsYs907kRbskDfAE9Rgmftpbbfy3ZbUX3JuIkDGtmlCgdfGkKoEUI3wNM0OEERB82/oyt4Kb0wdB04t89/1O/w1cDnyilFU='
+//             })
+//         }
+//     )
+//     .then(function(response){
+//         console.log('getwithNo_cor - success', response.json());
+//     })
+//     .catch(function(error){
+//         console.log('getwithNo_cor - error', error);
+//     });
+// }
 
-function getwith_cors(){
-    fetch('https://api.line.me/v2/bot/profile/U9629b9b72e84a8b4f4f904e21db16199', {	
-            'mode': 'cors',
-            headers: new Headers({
-                'Authorization': 'Bearer NCUUPMI9eL++OnIxuXLB4UFgn1I6BGR8MP6ljLJt2EWpNVRxjRZgfgMZdarheJ5d1Kd43O0V3RZkrsYs907kRbskDfAE9Rgmftpbbfy3ZbUX3JuIkDGtmlCgdfGkKoEUI3wNM0OEERB82/oyt4Kb0wdB04t89/1O/w1cDnyilFU='
-            })
-        }
-    )
-    .then(function(response){
-        console.log('getwith_cors - success', response.json());
-    })
-    .catch(function(error){
-        console.log('getwith_cors - error', error);
-    });
-}
+// function getwith_cors(){
+//     fetch('https://api.line.me/v2/bot/profile/U9629b9b72e84a8b4f4f904e21db16199', {	
+//             'mode': 'cors',
+//             headers: new Headers({
+//                 'Authorization': 'Bearer NCUUPMI9eL++OnIxuXLB4UFgn1I6BGR8MP6ljLJt2EWpNVRxjRZgfgMZdarheJ5d1Kd43O0V3RZkrsYs907kRbskDfAE9Rgmftpbbfy3ZbUX3JuIkDGtmlCgdfGkKoEUI3wNM0OEERB82/oyt4Kb0wdB04t89/1O/w1cDnyilFU='
+//             })
+//         }
+//     )
+//     .then(function(response){
+//         console.log('getwith_cors - success', response.json());
+//     })
+//     .catch(function(error){
+//         console.log('getwith_cors - error', error);
+//     });
+// }
 
-getwithNo_cors();
-getwith_cors();
+// getwithNo_cors();
+// getwith_cors();
