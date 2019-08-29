571ca6c4fa2f84174b8823d506b13107c68ac9ad
Cheevavorn Saitthakool <mean.mea2@gmail.com>
2019-08-30 00:39:05 +0700

    add ga

--- partials/ga_tags.php
+++ partials/ga_tags.php
@@ -1,24 +1,9 @@
 <!-- Global site tag (gtag.js) - Google Analytics -->
-<!-- <script async src="https://www.googletagmanager.com/gtag/js?id=UA-144567539-1"></script>
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-144567539-1"></script>
 <script>
   window.dataLayer = window.dataLayer || [];
   function gtag(){dataLayer.push(arguments);}
   gtag('js', new Date());
 
   gtag('config', 'UA-144567539-1');
-</script> -->
-
-<script type="text/javascript">
-  var _gauges = _gauges || [];
-  (function() {
-    var t   = document.createElement('script');
-    t.type  = 'text/javascript';
-    t.async = true;
-    t.id    = 'gauges-tracker';
-    t.setAttribute('data-site-id', '5d4cd6bb1c79212006712ae6');
-    t.setAttribute('data-track-path', 'https://track.gaug.es/track.gif');
-    t.src = 'https://d2fuc4clr7gvcn.cloudfront.net/track.js';
-    var s = document.getElementsByTagName('script')[0];
-    s.parentNode.insertBefore(t, s);
-  })();
 </script>
