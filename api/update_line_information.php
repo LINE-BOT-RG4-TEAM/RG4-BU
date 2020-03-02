0809f9e329b1202a79d3b0a9e70b04ada3037c3c
Cheevavorn Saitthakool <mean.mea2@gmail.com>
2020-03-02 23:27:35 +0700

    update register timestamp while customer registed

--- api/update_line_information.php
+++ api/update_line_information.php
@@ -1,4 +1,5 @@
 <?php
+  date_default_timezone_set("Asia/Bangkok");
   require('../utils/db_connector.php');
 
   $caInput = $_POST['caInput'];
@@ -9,14 +10,18 @@
   $telInput = $_POST['telInput'];
   $emailInput = $_POST['emailInput'];
 
+  // create current timestamp
+  $date = date();
+  $current_timestamp = date_format($date,"Y/m/d H:i:s");
+
   $update_line = "
     UPDATE ca
-    SET UserID = ?, FullName = ?, CA_TEL = ?, CA_EMAIL = ?
+    SET UserID = ?, FullName = ?, CA_TEL = ?, CA_EMAIL = ?, Register_Timestamp = ?
     WHERE CA = ?
   ";
 
   $stmt = $conn->prepare($update_line);
-  $stmt->bind_param("sssss", $uIdInput, $nameInput, $telInput, $emailInput, $remove_zero_prefix_ca);
+  $stmt->bind_param("ssssss", $uIdInput, $nameInput, $telInput, $emailInput, $current_timestamp, $remove_zero_prefix_ca);
   $stmt->execute();
   if($stmt->error) {
     http_response_code(503);
